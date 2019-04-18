a895f868281ec6e15a2242798c4a9608eeb4cbf3
katerina freeman <katerinafreeman@gmail.com>
2019-04-18 15:28:01 -0500

    sdf

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const jsonServer = require('json-server');
 const server = jsonServer.create();
-const router = jsonServer.router('yak.json');
+const router = jsonServer.router('capstone.json');
 const middlewares = jsonServer.defaults();
 const port = process.env.PORT || 3000;
 
@@ -8,3 +8,5 @@ server.use(middlewares);
 server.use(router);
 
 server.listen(port);
+
+//change
