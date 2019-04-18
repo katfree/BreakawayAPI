f6fdbe97c854b1d3fc3b8ff5a711b2cec30ac25a
katerina freeman <katerinafreeman@gmail.com>
2019-04-18 14:36:52 -0500

    m

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
-const jsonServer = require('json-server')
-const server = jsonServer.create()
-const router = jsonServer.router('db.json')
-const middlewares = jsonServer.defaults()
+const jsonServer = require('json-server');
+const server = jsonServer.create();
+const router = jsonServer.router('yak.json');
+const middlewares = jsonServer.defaults();
+const port = process.env.PORT || 3000;
 
-server.use(middlewares)
-server.use(router)
-server.listen(process.env.PORT, () => {
-  console.log('JSON Server is running')
-})
+server.use(middlewares);
+server.use(router);
+
+server.listen(port);
